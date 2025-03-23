2f37240f650da2fcdd2ca7c2b00d04b58ced9ab7
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2025-03-23 20:27:23 +0000

    update👍:astro.config.mjsの設定に必要なものを追加
    - `site`にサイトのfqdnを追加
    - `server`内のの`host`のループバックアドレスをipv4に固定（こうしないとWSL2で動くdevcontainerでデバッグモードが正常に機能しなかった）

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://www.jan-s-watt.jp",
+    server: {
+        host: "127.0.0.1"
+    }
+});
